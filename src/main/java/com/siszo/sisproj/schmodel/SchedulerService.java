6ff6cf5660b80ca5eb0307ec73efd526dfecd555
java <java@java-PC>
2017-12-18 19:47:30 +0900

    [2017-12-18 19:46 김성규] 스케줄러 삭제처리, 등록 된 스케줄 드래그햇을 시 수정처리 성공, vo수정

--- src/main/java/com/siszo/sisproj/schmodel/SchedulerService.java
+++ src/main/java/com/siszo/sisproj/schmodel/SchedulerService.java
@@ -5,4 +5,6 @@ import java.util.List;
 public interface SchedulerService {
 	public int schedulerInsert(SchedulerVO vo);
 	public List<SchedulerVO> schedulerSelectAll(int empNo);
+	public int schedulerDelete(String pschNo);
+	public int schedulerUpdate(SchedulerVO vo);
 }
