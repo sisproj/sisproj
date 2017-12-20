a9c814bae198977cc60eefd94b4d3094c4a36799
java <java@java-PC>
2017-12-20 15:23:08 +0900

    [2017-12-20 15:22 오영] 파일최신화 충돌 대비

--- src/main/java/com/siszo/sisproj/notice/controller/NoticeController.java
+++ src/main/java/com/siszo/sisproj/notice/controller/NoticeController.java
@@ -2,8 +2,13 @@ package com.siszo.sisproj.notice.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.siszo.sisproj.notice.model.NoticeService;
 
 @Controller
 @RequestMapping("/notice")
@@ -11,14 +16,26 @@ public class NoticeController {
 	private static final Logger logger
 		= LoggerFactory.getLogger(NoticeController.class);
 	
+	/*@Autowired
+	private NoticeService noticeService;*/
+	
+	@RequestMapping(value="/noticeWrite.do", method=RequestMethod.GET)
+	public String noticeWrite_get() {
+		logger.info("공지사항 글쓰기");
+		
+		return "notice/noticeWrite";
+	}
+	
+	@RequestMapping(value="/noticeWrite.do", method=RequestMethod.POST)
+	public String noticeWrite_post() {
+		logger.info("공지사항 글쓰기");
+		
+		return "common/message";	
+	}
+	
 	@RequestMapping("/noticeList.do")
 	public void noticeList() {
 		logger.info("공지사항 리스트");
-	}
-	
-	@RequestMapping("/noticeWrite.do")
-	public void noticeWrite() {
-		logger.info("공지사항 글쓰기");
 	}
 	
 	@RequestMapping("/noticeUpdate.do")
