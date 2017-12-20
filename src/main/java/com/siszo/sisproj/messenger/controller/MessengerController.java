b200ac18675161bba978020f5417b3a7a0f2e472
김영재 <bing9870@gmail.com>
2017-12-20 20:00:01 +0900

    [2017-12-20 20:00 김영재] 채팅 시작하기 화면 수정

--- src/main/java/com/siszo/sisproj/messenger/controller/MessengerController.java
+++ src/main/java/com/siszo/sisproj/messenger/controller/MessengerController.java
@@ -1,17 +1,28 @@
 package com.siszo.sisproj.messenger.controller;
 
-
+import com.siszo.sisproj.dept.model.DeptService;
+import com.siszo.sisproj.dept.model.DeptVO;
+import com.siszo.sisproj.employee.model.EmployeeVO;
+import com.siszo.sisproj.organization.model.OrganizationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 public class MessengerController {
     private static final Logger logger = LoggerFactory.getLogger(MessengerController.class);
+
+    @Autowired
+    private DeptService deptService;
+    @Autowired
+    private OrganizationService orgService;
 
 
     @RequestMapping("/messenger/messenger.do")
@@ -26,7 +37,18 @@ public class MessengerController {
     }
 
     @RequestMapping(value = "/messenger/messengerStart.do", method = RequestMethod.GET)
-    public String messengerStart_get() {
+    public String messengerStart_get(@RequestParam(defaultValue = "0") int deptNo, Model model) {
+        logger.info("조직도 부서명 리스트 and 부서별 사원리스트, 파라미터 deptNo={}", deptNo);
+
+        List<DeptVO> deptList = deptService.selectDeptName();
+        logger.info("부서명 조회결과 deptList.size()={}", deptList.size());
+
+        List<EmployeeVO> empList = orgService.selectEmp();
+        logger.info("부서별 사원리스트 조회 결과 empList.size()={}", empList.size());
+
+        model.addAttribute("deptList", deptList);
+        model.addAttribute("empList", empList);
+
         return "messenger/messengerStart";
     }
 }
