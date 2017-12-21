57c8142e55b9e3280a839ea674e64b680669e0dc
java <java@java-PC>
2017-12-21 10:06:00 +0900

    [2017-12-21 10:04 오영]컨트롤러 dao service vo 오류 수정사항

---
+++ src/main/java/com/siszo/sisproj/notice/model/NoticeServiceImpl.java
@@ -0,0 +1,35 @@
+package com.siszo.sisproj.notice.model;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.siszo.sisproj.common.SearchVO;
+
+@Service
+public class NoticeServiceImpl implements NoticeService{
+	private static final Logger logger
+	=LoggerFactory.getLogger(NoticeServiceImpl.class);
+	
+	@Autowired
+	private NoticeDAO noticeDao;
+	
+	@Override
+	public int insertNotice(NoticeVO vo) {
+		return noticeDao.insertNotice(vo);
+	}
+
+	@Override
+	public List<NoticeVO> selectAll(SearchVO searchVo) {
+		return noticeDao.selectAll(searchVo);
+	}
+
+	@Override
+	public int selectTotalRecordCount(SearchVO searchVo) {
+		return noticeDao.selectTotalRecordCount(searchVo);
+	}
+
+}
