ff62b2d40d14db804c9e8271426d5c2aea2e0371
손종훈 <mabus23@naver.com>
2018-01-04 20:00:15 +0900

    [2018-01-04 19:58 손종훈]자원관리 승인반려대기 기능 추가

--- src/main/java/com/siszo/sisproj/reservation/controller/ReservationController.java
+++ src/main/java/com/siszo/sisproj/reservation/controller/ReservationController.java
@@ -1,5 +1,8 @@
 package com.siszo.sisproj.reservation.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
@@ -9,6 +12,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.siszo.sisproj.common.PaginationInfo;
 import com.siszo.sisproj.common.Utility;
@@ -48,8 +53,8 @@ public class ReservationController {
 		return "common/message";
 	}
 
-	@RequestMapping("/requsetList.do")
-	public void requestList(@ModelAttribute ReservationSearchVO searchVo,Model model) {
+	@RequestMapping("/requestList.do")
+	public String requestList(@ModelAttribute ReservationSearchVO searchVo,Model model) {
 		logger.info("승인 대기중인 자원예약 리스트, 파라미터 searchVo={}", searchVo);
 		
 		//Paging 처리에 필요한 변수를 계산해주는 PaginationInfo 생성
@@ -63,13 +68,58 @@ public class ReservationController {
 		searchVo.setFirstRecordIndex(pagingInfo.getFirstRecordIndex());
 		logger.info("searchVo 최종값 : {}", searchVo);
 		
+		List<Map<String, Object>> list=resService.selectReservationAll(searchVo);
+		logger.info("승인 대기중인 자원예약 리스트 조회결과 list.size={}", list.size());		
 		
-		
-		
-		int totalRecord = 0;
+		int totalRecord=resService.selectTotalRecord();
 		logger.info("승인대기 전체 개수 조회 결과, totalRecord={}", totalRecord);
 		
 		pagingInfo.setTotalRecord(totalRecord);
 
+		model.addAttribute("list", list);
+		model.addAttribute("pagingInfo", pagingInfo);
+		
+		return "resource/requestList";
 	}
+	
+	@RequestMapping("/confirmY.do")
+	public String updateConfirmY(@RequestParam(defaultValue="0") int rvNo, Model model) {
+		logger.info("자원 예약 승인 파라미터 rvNo={}",rvNo);
+		
+		int cnt=resService.updateConfirmY(rvNo);
+		logger.info("자원 예약 승인 결과 cnt={}", cnt);
+		
+		String msg="",url="/resource/requestList.do";
+		if(cnt>0) {
+			msg="예약이 승인 성공";
+		}else {
+			msg="예약이 승인 실패";			
+		}
+		
+		model.addAttribute("msg", msg);
+		model.addAttribute("url", url);
+		
+		return "common/message";
+	}
+	
+	@RequestMapping("/confirmR.do")
+	public String updateConfirmR(@RequestParam(defaultValue="0") int rvNo, Model model) {
+		logger.info("자원 예약 반려 파라미터 rvNo={}",rvNo);
+		
+		int cnt=resService.updateConfirmR(rvNo);
+		logger.info("자원 예약 반려 결과 cnt={}", cnt);
+		
+		String msg="",url="/resource/requestList.do";
+		if(cnt>0) {
+			msg="예약 반려 성공";
+		}else {
+			msg="예약 반려 실패";			
+		}
+		
+		model.addAttribute("msg", msg);
+		model.addAttribute("url", url);
+		
+		return "common/message";
+	}
+
 }
